6ca065277d62e42de03e61856a14ae3648a32fb8
Tarinaky <tarinaky@gmail.com>
2012-10-15 18:47:54 +0100

    Have a Word testcase. Build now working.

---
+++ src/uk/ac/aber/cs211/wordladder/tests/WordTest.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package uk.ac.aber.cs211.wordladder.tests;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jacob Smith, jas32
+ *
+ */
+public class WordTest extends TestCase {
+	
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link uk.ac.aber.cs211.wordladder.Word#getString()}.
+	 */
+	@Test
+	public void testGetString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link uk.ac.aber.cs211.wordladder.Word#isSimilar(java.lang.String)}.
+	 */
+	@Test
+	public void testIsSimilar() {
+		fail("Not yet implemented");
+	}
+
+}
